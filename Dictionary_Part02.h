000752fd8a952982f09c17ed414dde652f9166d9
EzraPadman <84053625+EzraPadman@users.noreply.github.com>
2021-05-12 19:46:21 +0800

    Update Dictionary_Part02.h

--- Dictionary_Part02.h
+++ Dictionary_Part02.h
@@ -1,16 +1,50 @@
-#pragma once
 
-
-#ifndef DICTIONARY_PART02_H
-#define DICTIONARY_PART02_H
 #include <iostream>
 #include <string>
 #include "Dictionary.h"
-#include "Word.h"
+#include "Dictionary_Part02.h"
 
-class Dictionary_Part02 : public Dictionary
+
+using namespace std;
+
+Dictionary_Part02::Dictionary_Part02(): Dictionary() {}
+
+void Dictionary_Part02::palindrome()
 {
-public:
+	int i, length;
+	int flag = 0;
 
+	for (Word current : theWords)
+	{
+		string test_word = current.getWord();
+		length = strlen(test_word.c_str());
+
+		for (i = 0; i < length; i++)
+		{
+			if (test_word[i] != test_word[length - i - 1])
+			{
+				flag = 1;
+				break;
+			}
+		}
+		if (flag) {
+			break;
+		}
+		else {
+			cout << test_word << "is a palindrome" << endl;
+		}
+	}
+
+
+}
+
+void Dictionary_Part02::noun_and_verb()
+{
+}
+
+void Dictionary_Part02::anagrams() 
+{
+}
 	
-};
+
+
