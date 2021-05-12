4610829a17dbf0a16db2d01577036135b2b1d4d1
EzraPadman <84053625+EzraPadman@users.noreply.github.com>
2021-05-12 09:37:57 +0800

    Create Dictionary_Part02.h

---
+++ Dictionary_Part02.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+#ifndef DICTIONARY_PART02_H
+#define DICTIONARY_PART02_H
+#include <iostream>
+#include <string>
+#include "Dictionary.h"
+#include "Word.h"
+
+class Dictionary_Part02 : public Dictionary
+{
+public:
+
+	
+};
