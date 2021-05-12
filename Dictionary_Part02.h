c7311825226a6ef7722f07e7dcd6a44cbd6ce2c6
EzraPadman <84053625+EzraPadman@users.noreply.github.com>
2021-05-12 19:49:46 +0800

    Create Dictionary_Part02.h

---
+++ Dictionary_Part02.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Dictionary.h"
+
+
+
+class Dictionary_Part02 : public Dictionary
+{
+public:
+	
+	Dictionary_Part02();
+
+	void palindrome();
+	void noun_and_verb();
+	void anagrams();
+	
+
+};
